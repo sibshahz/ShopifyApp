c2ee35a6a6d22e2423f52be8cffc169c04b444d0
Shahid Gillani <shahid5ssg@gmail.com>
2022-02-27 20:20:53 +0500

    Adding all files to git

---
+++ components/ProductPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Page} from "@shopify/polaris";
+import ProductList from "./ProductList";
+
+function ProductPage({setIsOpen, products}){
+    return(
+        <Page 
+      title="Product Selector"
+      primaryAction={{
+        content:"Select product",
+        onAction: ()=>setIsOpen(true),
+      }}
+      >
+        
+        <ProductList products={products} />
+        
+      </Page>
+    );
+}
+
+export default ProductPage;
